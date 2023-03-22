034f36568658d859a4c40e9bdc6e62cf53afa479
dhyan_16k <dhyank16@gmail.com>
2023-03-22 15:41:41 +0530

    mac to windows
    started working on my system

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Dhyan Kundhadia\Documents\GitHub\voxify\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
